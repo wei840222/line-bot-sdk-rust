7ee3768d91949836e4d75b1d81abc5ffe1fd13b5
wei <wei840222@gmail.com>
2020-08-28 19:35:42 +0800

    add check_signature in sample

--- example/echo/src/main.rs
+++ example/echo/src/main.rs
@@ -2,11 +2,56 @@
 #[macro_use]
 extern crate rocket;
 use line_bot_sdk_rust::{bot, model};
-use rocket_contrib::json::Json;
+use rocket::data::{self, FromDataSimple};
+use rocket::http::Status;
+use rocket::request::{self, FromRequest, Request};
+use rocket::Data;
+use rocket::Outcome::{Failure, Success};
+use std::io::Read;
 
-#[post("/callback", format = "json", data = "<web_hook>")]
-fn callback(web_hook: Json<model::WebHook>) -> &'static str {
-  let bot_client = bot::LineBot::new(std::env::var("CHANNEL_TOKEN").unwrap());
+pub struct Signature {
+  pub key: String,
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for Signature {
+  type Error = ();
+  fn from_request(request: &'a Request<'r>) -> request::Outcome<Signature, ()> {
+    let keys: Vec<_> = request.headers().get("X-Line-Signature").collect();
+    if keys.len() != 1 {
+      return Failure((Status::BadRequest, ()));
+    }
+    Success(Signature {
+      key: keys[0].to_string(),
+    })
+  }
+}
+
+pub struct Body {
+  pub data: String,
+}
+
+impl FromDataSimple for Body {
+  type Error = String;
+  fn from_data(_: &Request, data: Data) -> data::Outcome<Self, String> {
+    let mut string = String::new();
+    if let Err(e) = data.open().read_to_string(&mut string) {
+      return Failure((Status::InternalServerError, format!("{:?}", e)));
+    }
+    Success(Body { data: string })
+  }
+}
+
+#[post("/callback", format = "json", data = "<body>")]
+fn callback(signature: Signature, body: Body) -> Status {
+  let bot_client = bot::LineBot::new(
+    std::env::var("CHANNEL_TOKEN").unwrap(),
+    std::env::var("CHANNEL_SECRET").unwrap(),
+  );
+  if !bot_client.check_signature(&body.data, &signature.key) {
+    return Status::BadRequest;
+  }
+
+  let web_hook: model::WebHook = serde_json::from_str(&body.data).unwrap();
   for event in web_hook.events.iter() {
     match event.r#type {
       "follow" | "join" => bot_client.reply_message(
@@ -36,7 +81,7 @@ fn callback(web_hook: Json<model::WebHook>) -> &'static str {
       _ => {}
     }
   }
-  "OK"
+  Status::Ok
 }
 
 fn main() {
