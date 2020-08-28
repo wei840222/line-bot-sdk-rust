7b117bb9a63a318598417a58ee502949d3cd7d7a
wei <wei840222@gmail.com>
2020-08-28 18:35:39 +0800

    add check_signature helper function

--- src/bot.rs
+++ src/bot.rs
@@ -1,14 +1,25 @@
 use crate::model;
+use base64::encode;
+use hmac::{Hmac, Mac, NewMac};
+use sha2::Sha256;
 
 pub struct LineBot {
     pub channel_token: String,
+    pub channel_secret: String,
 }
 
 impl LineBot {
-    pub fn new(channel_token: String) -> LineBot {
+    pub fn new(channel_token: String, channel_secret: String) -> LineBot {
         LineBot {
             channel_token: channel_token,
+            channel_secret: channel_secret,
         }
+    }
+
+    pub fn check_signature(&self, body: &str, signature: &str) -> bool {
+        let mut mac = Hmac::<Sha256>::new_varkey(self.channel_secret.as_bytes()).unwrap();
+        mac.update(body.as_bytes());
+        signature == encode(&mac.finalize().into_bytes().to_vec())
     }
 
     pub fn reply_message(&self, event: &model::Event, messages: serde_json::Value) {
